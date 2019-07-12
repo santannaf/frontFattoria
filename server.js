27126644db87518189a0458c5e22ad9ce5016913
thalesfariassantanna <thales.farias.santanna@gmail.com>
2019-07-12 03:10:27 -0300

    build

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const path = require('path');
 
 app.use(express.static(__dirname + '/dist'));
 
+
+
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/fattoria/index.html'));
 })
 
 console.log('Console listenning');
