e51eb4ba962500e1f720ad80e6442d239b6685b2
santannaf <thales.farias.santanna@gmail.com>
2019-07-13 17:23:36 -0300

    spinner test

--- src/app/urls/urls.module.ts
+++ src/app/urls/urls.module.ts
@@ -1,18 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { UrlsRoutingModule } from './urls-routing.module';
-import { UrlsListaComponent } from './urls-lista/urls-lista.component';
-import { UrlsFormComponent } from './urls-form/urls-form.component';
-import { ReactiveFormsModule } from '@angular/forms'
-
+import { UrlsRoutingModule } from "./urls-routing.module";
+import { UrlsListaComponent } from "./urls-lista/urls-lista.component";
+import { UrlsFormComponent } from "./urls-form/urls-form.component";
+import { ReactiveFormsModule } from "@angular/forms";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 
 @NgModule({
   declarations: [UrlsListaComponent, UrlsFormComponent],
   imports: [
     CommonModule,
     UrlsRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatProgressSpinnerModule
   ]
 })
-export class UrlsModule { }
+export class UrlsModule {}
